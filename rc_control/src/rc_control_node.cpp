7bf8016407c5085151ba879d6491c065f3da55be
Zixu Zhang <zixuz@princeton.edu>
2022-12-05 15:30:58 -0500

    inital commit to this repo

---
+++ rc_control/src/rc_control_node.cpp
@@ -0,0 +1,40 @@
+#include <ros/ros.h>
+#include <ros/console.h>
+
+#include <signal.h>
+#include <servo_controller.h>
+
+
+
+void signalHandler(int sig) {
+    ROS_INFO("Controller shutdown complete. Calling ros shutdown.");
+    ros::shutdown();
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "rc_controller_node", ros::init_options::NoSigintHandler);
+    ros::NodeHandle node;    
+    ros::NodeHandle private_nh("~");
+    signal(SIGINT, signalHandler);
+
+    ServoController controller(node, private_nh);
+
+    double freq;
+    private_nh.getParam("frequency", freq);
+
+    const ros::Duration dt(1.0/freq);
+    ros::Rate rate(freq);
+
+    // start the driver
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+    while(ros::ok())
+    {
+        controller.update(ros::Time::now(), dt);
+        rate.sleep();
+    }
+    spinner.stop();
+    
+    return 0;
+}
